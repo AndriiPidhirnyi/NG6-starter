5ccdb1826610af91a30e40d907c9326ed87fb100
Andrii Pidhirnyi <andrii_pidhirnyi@epam.com>
2016-06-06 22:47:18 +0300

    Formatting template files

--- generator/component/temp.js
+++ generator/component/temp.js
@@ -1,5 +1,5 @@
-import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import angular              from 'angular';
+import uiRouter             from 'angular-ui-router';
 import <%= name %>Component from './<%= name %>.component';
 
 let <%= name %>Module = angular.module('<%= name %>', [
--- generator/component/temp.spec.js
+++ generator/component/temp.spec.js
@@ -1,14 +1,13 @@
-import <%= upCaseName %>Module from './<%= name %>'
+import <%= upCaseName %>Module     from './<%= name %>'
 import <%= upCaseName %>Controller from './<%= name %>.controller';
-import <%= upCaseName %>Component from './<%= name %>.component';
-import <%= upCaseName %>Template from './<%= name %>.html';
+import <%= upCaseName %>Component  from './<%= name %>.component';
+import <%= upCaseName %>Template   from './<%= name %>.html';
 
 describe('<%= upCaseName %>', () => {
-  let $rootScope, makeController;
+  let makeController;
 
   beforeEach(window.module(<%= upCaseName %>Module.name));
-  beforeEach(inject((_$rootScope_) => {
-    $rootScope = _$rootScope_;
+  beforeEach(inject(() => {
     makeController = () => {
       return new <%= upCaseName %>Controller();
     };
@@ -22,7 +21,7 @@ describe('<%= upCaseName %>', () => {
     // controller specs
     it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
       let controller = makeController();
-      expect(controller).to.have.property('name');
+      expect(controller.name).toBeDefined();
     });
   });
 
@@ -30,24 +29,24 @@ describe('<%= upCaseName %>', () => {
     // template specs
     // tip: use regex to ensure correct bindings are used e.g., {{  }}
     it('has name in template [REMOVE]', () => {
-      expect(<%= upCaseName %>Template).to.match(/{{\s?vm\.name\s?}}/g);
+      expect(<%= upCaseName %>Template).toMatch(/{{\s?vm\.name\s?}}/g);
     });
   });
 
   describe('Component', () => {
-      // component/directive specs
-      let component = <%= upCaseName %>Component;
+    // component/directive specs
+    let component = <%= upCaseName %>Component;
 
-      it('includes the intended template',() => {
-        expect(component.template).to.equal(<%= upCaseName %>Template);
-      });
+    it('includes the intended template', () => {
+      expect(component.template).toEqual(<%= upCaseName %>Template);
+    });
 
-      it('uses `controllerAs` syntax', () => {
-        expect(component).to.have.property('controllerAs');
-      });
+    it('uses `controllerAs` syntax', () => {
+      expect(component.controllerAs).toBeDefined();
+    });
 
-      it('invokes the right controller', () => {
-        expect(component.controller).to.equal(<%= upCaseName %>Controller);
-      });
+    it('invokes the right controller', () => {
+      expect(component.controller).toEqual(<%= upCaseName %>Controller);
+    });
   });
 });
